8475e99de14dc8b1fd1f90478b4515d3fb553c71
AnuragBorisa <anuragborisa@gmail.com>
2024-02-20 16:21:52 +0530

    changes

--- client/src/components/Utilites/InfiniteSilder/InfiniteSlider.jsx
+++ client/src/components/Utilites/InfiniteSilder/InfiniteSlider.jsx
@@ -6,13 +6,11 @@ import logofour from "./Resources/Logos/PngItem_1320262.png";
 import "./InfiniteSlider.css";
 
 const logos = [
-  { id: 1, src: "https://res.cloudinary.com/dhuagtzvw/image/upload/v1708413492/images/yogpqfpluyh4dkpc4gbd.png", alt: "Logo 1" },
-  { id: 1, src: "https://res.cloudinary.com/dhuagtzvw/image/upload/v1708413492/images/tyi8ldzbypaoau0zsjgf.png", alt: "Logo 1" },
-  { id: 1, src: "https://res.cloudinary.com/dhuagtzvw/image/upload/v1708413492/images/voiow5uudfmln2m9ohme.png", alt: "Logo 1" },
-  { id: 1, src: "https://res.cloudinary.com/dhuagtzvw/image/upload/v1708413492/images/p69wiadjrz80cdhxt7iu.png", alt: "Logo 1" },
-  { id: 1, src: "https://res.cloudinary.com/dhuagtzvw/image/upload/v1708413492/images/yogpqfpluyh4dkpc4gbd.png", alt: "Logo 1" },
- 
-  
+  { id: 1, src: logone, alt: "Logo 1" },
+  { id: 1, src: logotwo, alt: "Logo 1" },
+  { id: 1, src: logothree, alt: "Logo 1" },
+  { id: 1, src: logofour, alt: "Logo 1" },
+  { id: 1, src: logothree, alt: "Logo 1" },
 ];
 const carouselElements = logos.map((logo, index) => {
   return (<>
@@ -20,6 +18,7 @@ const carouselElements = logos.map((logo, index) => {
       <img src={logo.src} alt={logo.alt} className="slider-img"></img>
       
     </div>
+   
    
    </>
   );
